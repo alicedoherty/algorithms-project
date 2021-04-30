4c33c327ba3db0443c9b80866329b177fd3cee8d
alicedoherty <66687032+alicedoherty@users.noreply.github.com>
2021-04-30 18:55:27 +0100

    Cleaning up getting user input for shortest path

--- src/BusManagementSystem.java
+++ src/BusManagementSystem.java
@@ -1,52 +1,32 @@
-//4. Provide front interface enabling selection between the above features or an option to exit
-//the programme, and enabling required user input. It does not matter if this is command-line
-//or graphical, as long as functionality/error checking is provided.
-//You are required to provide error checking and show appropriate messages in the case of erroneous
-//inputs – eg bus stop doesn’t exist, wrong format for time for bus stop (eg letters instead of
-//numbers), no route possible etc. 
-
-
-/* Selection between:
- 	1. Finding shortest path
- 		- Input
- 	2. Searching bus stop
- 		- Input
- 	3. Searching for trips in given time
- 		- Input
- 	4. Exit
- 	
- 	Error messages
-*/  
-
-
 import java.util.Scanner;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.text.ParseException;
 
 public class BusManagementSystem {
-
-	   public static void main(String[] args){
+		
+		public static void main(String[] args){
 		   Scanner input = new Scanner(System.in);
 		   boolean validInput = false;
 		   
 		   while(!validInput) {
-			   System.out.println("Choose which feature you want (0, 1, 2 or 3):\n"
+			   System.out.println("\nChoose which feature you want (0, 1, 2 or 3):\n"
 			   		+ "0: Quit\n"
 			   		+ "1: Shortest path between two bus stops\n"
 			   		+ "2: Search for a bus stop\n"
-			   		+ "3: Search for trips within a given time");
-			   
+			   		+ "3: Search for trips within a given time\n");
+
 			   if(input.hasNextInt()) {
 				   int userChoice = input.nextInt();
+	
 				   switch(userChoice) {
 				   		case 0:
 				   			validInput = true;
-				   			System.out.println("case 0");
+				   			System.out.println("Goodbye :)");
 				   			break;
 				   		case 1:
 				   			validInput = true;
-				   			System.out.println("case 1");
+				   			callGetShortestPath();
 				   			break;
 				   		case 2:
 				   			validInput = true;
@@ -60,18 +40,17 @@ public class BusManagementSystem {
 				   			System.out.println("Choice not valid.");	
 				   
 				   }
-				   
 			   }
 			   
 			   else {
 				   System.out.println("Please only enter numbers.");
-				   input.next();
-			   }
+				   input.nextLine();
+			  }
 			   
 		   }
-		   input.close();
+		  input.close();
 		   		        
-	   }	
+	   }
 	   
 	   public static void callSearchArrivalTime() {
 		   Scanner userInput = new Scanner(System.in);
@@ -94,6 +73,7 @@ public class BusManagementSystem {
 			   
 		   }
 		   userInput.close();
+		   
 	   }
 	   
 	   public static void callBusStopSearch() {
@@ -103,7 +83,13 @@ public class BusManagementSystem {
 		   String inputStop = userInput.nextLine();
 			   
 		   BusStopSearch bss = new BusStopSearch("stops.txt");
-		   bss.displayStopDetails(inputStop);
+		   bss.displayStopDetails(inputStop.toUpperCase());
+		   userInput.close();
+	   }
+	   
+	   public static void callGetShortestPath() {
+		   Shortest_Path_Between_2_Stops.getShortestPath();
+		   
 	   }
 
 }
